eef425c3f93b7d775ce1d844800bc850b58a2db4
Thomas Deutsch <tomdeu@gmail.com>
2018-11-07 00:29:36 +0100

    Create HelloWorld.java

---
+++ src/main/java/com/tcmj/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.tcmj.test;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    try {
+      System.out.println("Hello tcmj pug world!");
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+}
